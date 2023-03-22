667e2a45497245be0f905e120a0a8308a100b8dd
Pibob73 <114067850+Pibob73@users.noreply.github.com>
2023-03-22 19:53:43 +0300

    Delete srcServer directory

--- srcServer/patternsWrite.js
+++
@@ -1,8 +0,0 @@
-class WriteToFile {
-  constructor(article) {
-    this.format = `${article.title},${article.author},${article.text}`;
-    return this.format;
-  }
-}
-
-module.exports = { WriteToFile };
